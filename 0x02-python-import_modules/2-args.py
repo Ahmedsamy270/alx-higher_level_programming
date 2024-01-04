2fb737b409048ea381a1b71cdd157790ecb64e62
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-01-04 20:25:40 +0200

    ${var}

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,6 @@ if __name__ == '__main__':
     number = len(argv) - 1
     if number == 0:
         print("0 arguments.")
-    elif number == 1:
-        print("1 argument:")
     else:
         print("{} argument:".format(number))
         for i in range(number):
