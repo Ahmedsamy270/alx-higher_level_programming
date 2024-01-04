0fb06d1d64d39ee7bfe605d450a0d9ae66d93a5c
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-01-04 20:30:06 +0200

    ${var}

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,10 @@ if __name__ == '__main__':
     number = len(argv) - 1
     if number == 0:
         print("0 arguments.")
+    elif number == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
-        print("{} argument:".format(number))
+        print("{} arguments:".format(number))
         for i in range(number):
             print("{}: {}".format((i + 1), argv[i + 1]))
