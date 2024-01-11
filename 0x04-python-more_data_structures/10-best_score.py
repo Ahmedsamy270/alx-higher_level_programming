d398fc3fd9d1d0edd5bdcf04ba338a24f2d2e4f5
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-01-11 02:39:52 +0200

    ${var}

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary:
+        new = list(a_dictionary.keys())
+        name = ""
+        score = 0
+        for i in new:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                name = i
+        return name
