3900dfeb6a1368268cd8ee0c300676478c0ae9aa
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-01-23 22:36:32 +0200

    ${var}

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""defining a class Square"""
 
+
+class Square:
+    """represent a Square"""
+
+    def __init__(self, size):
+        """initializing a new Square
+
+        args:
+            size (int): the size of Square"""
+
+        self.__size = size
