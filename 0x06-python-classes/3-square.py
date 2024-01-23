5ac75b2fdf7e7328351f35d716c630f6a9463e55
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-01-23 22:49:24 +0200

    ${var}

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""defining a class Square"""
 
+
+class Square:
+    """represent a Square"""
+
+    def __init__(self, size=0):
+        """initializing a new Square
+
+        args:
+            size (int): the size of Square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """return the area of Square"""
+        return (self.__size * self.__size)
