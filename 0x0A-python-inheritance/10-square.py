167c4b2ae3c8035a608b817bb669a24c3b1ea995
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-05 22:35:00 +0200

    ${var}

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-def __init__(self, size):
+"""sefine rectangle subclass """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """represent square"""
+    def __init__(self, size):
+        """initialization new square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""sefine rectangle subclass """
+Rectangle = __import__('9-rectangle').Rectangle
 
+
+class Square(Rectangle):
+    """represent square"""
+    def __init__(self, size):
+        """initialization new square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """method for area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """return string represent of square"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
