fdd1b6cefabf6d5a1c75b9b0fb5d317fde63c5b6
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-05 18:40:29 +0200

    ${var}

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,8 +3,11 @@
 
 
 class MyList(list):
+    """ the subclass of list"""
     def __init__(self):
+        """ initalze object"""
         super().__init__()
 
     def print_sorted(self):
+        """print sorted list """
         print(sorted(self))
