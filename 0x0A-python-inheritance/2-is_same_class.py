ef4e75bec6d20e0d7ce173bc2cdf8d26f21f9a93
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-05 18:45:48 +0200

    ${var}

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""define a class checking func """
+
+
 def is_same_class(obj, a_class):
+    """check if an object is exactly an instance"""
+    if type(obj) == a_class:
+        return True
+    return False
