a57106690d269119e514ee64bed4c93ba0281164
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-07 00:12:26 +0200

    ${var}

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-def __init__(self, first_name, last_name, age):
-def to_json(self):
+"""define student class"""
+
+
+class Student:
+    """represent student """
+
+    def __init__(self, first_name, last_name, age):
+        """initialize new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dictionary represent of student"""
+        return self.__dict__
