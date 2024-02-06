20df52a518c96f7fb23ecd4e1ae734d50fefcd1c
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-06 23:52:50 +0200

    ${var}

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""define json to object func"""
+import json
+
+
 def from_json_string(my_str):
+    """retrurn python object represent"""
+    return json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""define json file writing func"""
+import json
+
+
 def save_to_json_file(my_obj, filename):
+    """write an object to txt file using json"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
