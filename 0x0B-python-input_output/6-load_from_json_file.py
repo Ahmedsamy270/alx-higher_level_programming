8ca848f8c74ba098d0985c2c914d93e06390e47c
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-07 00:03:57 +0200

    ${var}

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""define json file reading func"""
+import json
+
+
 def load_from_json_file(filename):
+    """creat python obj from json"""
+    with open(filename) as f:
+        return json.load(f)
