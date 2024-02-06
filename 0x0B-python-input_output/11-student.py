d9bce52bfd5dd7c4900d2a2846843c3d7ccb32b9
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-07 01:28:00 +0200

    ${var}

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""define student class"""
 
+
+class Student:
+    """represent student """
+
+    def __init__(self, first_name, last_name, age):
+        """initialize new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary represent of student"""
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace all attribute of student"""
+        for k, v in json.items():
+            setattr(self, k, v)
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+"""define a pascal triangle func"""
+
+
 def pascal_triangle(n):
+    """represent pascal triangle by size n"""
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        temp = [1]
+        for x in range(len(tri) - 1):
+            temp.append(tri[x] + tri[x + 1])
+        temp.append(1)
+        triangles.append(temp)
+    return triangles
