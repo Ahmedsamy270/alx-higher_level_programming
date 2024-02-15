bcc33ed11bd7fcf6d098d539dfbee67924d3f075
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-15 23:49:25 +0200

    ${var}

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""define the rectangle class """
+
+
+class Rectangle:
+    """this a class rectangle"""
+    def __init__(self, width=0, height=0):
+        """constractor initalzation """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """return width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise ValueError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """return height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if not isinstance(value, int):
+            raise ValueError("height must be an integer")
+        elif value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
