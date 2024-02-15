381ea47f855fae7899ab293644c49ba9d8f5f538
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-15 23:57:45 +0200

    ${var}

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,22 +6,8 @@ class Rectangle:
     """this a class rectangle"""
     def __init__(self, width=0, height=0):
         """constractor initalzation """
-        self.__width = width
-        self.__height = height
-
-    @property
-    def width(self):
-        """return width"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """width setter"""
-        if not isinstance(value, int):
-            raise ValueError("width must be an integer")
-        elif value < 0:
-            raise TypeError("width must be >= 0")
-        self.__width = value
+        self.width = width
+        self.height = height
 
     @property
     def height(self):
@@ -36,3 +22,17 @@ class Rectangle:
         elif value < 0:
             raise TypeError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        """return width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if not isinstance(value, int):
+            raise ValueError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
