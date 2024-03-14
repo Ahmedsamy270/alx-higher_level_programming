a92c91d64fff537e6c56065573c8b74a0bd44ccc
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-03-14 22:37:36 +0200

    ${var}

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,8 @@
 -- importing database dump from hbtn_0d_tvshows as task 15
-
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
